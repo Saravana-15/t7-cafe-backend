ad82ea7b6c337c4e49b0ad8a7618d10955aba581
Saravana-15 <saravanakumar.r@tthconsulting.com>
2025-07-12 16:21:06 +0530

    Update whatsapp.py

--- order_service/utils/whatsapp.py
+++ order_service/utils/whatsapp.py
@@ -7,7 +7,7 @@ load_dotenv()
 TWILIO_SID = "ACb72077cd4f7be33d06272fd6752f116a"   # os.getenv("TWILIO_SID")
 TWILIO_AUTH_TOKEN = "0d47b6b304d7eb73d052afd61a67ef15"  # os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_WHATSAPP_NUMBER = "whatsapp:+14155238886"  # Twilio sandbox number
-TO_WHATSAPP_NUMBER = "whatsapp:+917708441990"     # Your number
+TO_WHATSAPP_NUMBER = "whatsapp:+919655774432"     # Your number
 
 def send_whatsapp_message(message):
     try:
