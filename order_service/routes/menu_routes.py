d0eff5ccf2cc57ae04817ca1d1bbdd4deffd56d9
Saravana-15 <saravanakumar.r@tthconsulting.com>
2025-07-15 11:41:22 +0530

    Update menu_routes.py

--- order_service/routes/menu_routes.py
+++ order_service/routes/menu_routes.py
@@ -37,6 +37,48 @@ def get_menu():
             connection.close()'''
 
     menu_items = {
+          "Quick Bites": [
+            {"id": 1, "name": "Veg Puff", "price": 15, "image_url": "/static/images/veg-puff.jpg"},
+            {"id": 2, "name": "Egg Puff", "price": 18, "image_url": "/static/images/egg-puff.jpg"},
+            {"id": 3, "name": "Chicken Roll", "price": 22, "image_url": "/static/images/chicken-roll.jpg"},
+            {"id": 4, "name": "French Fries", "price": 45, "image_url": "/static/images/french-fries.jpg"},
+            {"id": 5, "name": "Smily Fries (5pcs)", "price": 45, "image_url": "/static/images/smiley-fries.jpg"},
+            {"id": 6, "name": "Cheese Ball (5pcs)", "price": 50, "image_url": "/static/images/cheese-ball.jpg"},
+            {"id": 7, "name": "Veg Nugget (5pcs)", "price": 45, "image_url": "/static/images/veg-nuggets.jpg"},
+            {"id": 8, "name": "Chicken Nugget (5pcs)", "price": 55, "image_url": "/static/images/chicken-nuggets.jpg"},
+            {"id": 9, "name": "Veg Momo (5pcs)", "price": 45, "image_url": "/static/images/veg-momo.jpg"},
+            {"id": 10, "name": "Chicken Momo (5pcs)", "price": 55, "image_url": "/static/images/chicken-momo.jpg"},
+            {"id": 11, "name": "Veg Cheese Sandwich", "price": 40, "image_url": "/static/images/veg-cheese-sandwich.jpg"},
+            {"id": 12, "name": "Paneer Sandwich", "price": 50, "image_url": "/static/images/paneer-sandwich.jpg"},
+            {"id": 13, "name": "Corn Sandwich", "price": 50, "image_url": "/static/images/corn-sandwich.jpg"},
+            {"id": 14, "name": "Bread Omelette", "price": 40, "image_url": "/static/images/bread-omelette.jpg"},
+            {"id": 15, "name": "Cheese Bread Omelette", "price": 50, "image_url": "/static/images/cheese-bread-omelette.jpg"},
+            {"id": 16, "name": "Veg Burger", "price": 60, "image_url": "/static/images/veg-burger.jpg"},
+            {"id": 17, "name": "Chicken Burger", "price": 80, "image_url": "/static/images/chicken-burger.jpg"}
+          ],
+          "Ice Creams": [
+            {"id": 18, "name": "Brownie with Ice Cream", "price": 70, "image_url": "/static/images/brownie-icecream.jpg"},
+            {"id": 19, "name": "Vanilla", "price": 40, "image_url": "/static/images/vanilla.jpg"},
+            {"id": 20, "name": "Strawberry", "price": 40, "image_url": "/static/images/strawberry.jpg"},
+            {"id": 21, "name": "Butterscotch", "price": 40, "image_url": "/static/images/butterscotch.jpg"},
+            {"id": 22, "name": "Chocolate", "price": 50, "image_url": "/static/images/chocolate.jpg"},
+            {"id": 23, "name": "Mango", "price": 50, "image_url": "/static/images/mango.jpg"},
+            {"id": 24, "name": "Black Currant", "price": 50, "image_url": "/static/images/blackcurrant.jpg"},
+            {"id": 25, "name": "Kulfi", "price": 40, "image_url": "/static/images/kulfi.jpg"}
+          ],
+          "Desserts": [
+            {"id": 26, "name": "Brownie", "price": 45, "image_url": "/static/images/brownie.jpg"},
+            {"id": 27, "name": "White Brownie", "price": 50, "image_url": "/static/images/white-brownie.jpg"},
+            {"id": 28, "name": "Mutta Mittai", "price": 45, "image_url": "/static/images/mutta-mittai.jpg"}
+          ],
+          "Cool Drinks": [
+            {"id": 29, "name": "Rose Milk", "price": 35, "image_url": "/static/images/rose-milk.jpg"},
+            {"id": 30, "name": "Badam Milk", "price": 35, "image_url": "/static/images/badam-milk.jpg"},
+            {"id": 31, "name": "Cold Coffee", "price": 40, "image_url": "/static/images/cold-coffee.jpg"}
+          ]
+        }
+
+    menu_itemss = {
         "Ice Creams": [
             {
                 "id": 13,
@@ -119,6 +161,11 @@ def get_menu():
                 "image_url": "/static/images/veg-sandwich.jpg",
                 "name": "Veg Sandwich",
                 "price": 40.0
+            }
+        ],
+        "Drinks": [
+            {
+                'image_url': ''
             }
         ]
     }
